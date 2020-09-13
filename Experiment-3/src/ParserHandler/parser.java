a8deeab2ab136aad42765ba91f0b35ab8ab5e51b
tetal <tetal@LAPTOP-24QJFU3M>
2020-09-13 21:16:41 +0530

    First Commit

--- Experiment-3/src/ParserHandler/parser.java
+++ Experiment-3/src/ParserHandler/parser.java
@@ -15,7 +15,7 @@ public class parser {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder = factory.newDocumentBuilder();
 	
-		Document document = builder.parse(new File("C:/Users/tetal/Desktop/CNWTLAB/Experiment-3/WebContent/User.xml"));
+		Document document = builder.parse(new File("C:\\Users\\tetal\\git\\Online-book-store-static-web-project\\Experiment-3\\WebContent\\User.xml"));
 		
 		Element root = document.getDocumentElement();
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
